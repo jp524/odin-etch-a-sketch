4fa04e0fb858a5479ad31cb3454d11f6d1f56896
JP <85654561+jp524@users.noreply.github.com>
2022-07-16 20:59:20 -0400

    Add squares for grid

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("JS test");
+const container = document.querySelector(".container");
+
+function createSquare() {
+  const square = document.createElement("div");
+  square.classList.add("square");
+  container.appendChild(square);
+}
+
+for (let i = 0; i < 16 * 16; i++) {
+  createSquare();
+};
