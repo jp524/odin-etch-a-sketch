a6d84653aba3bf33e7c7f6bc8c2994bc5d292a37
JP <85654561+jp524@users.noreply.github.com>
2022-07-18 07:50:50 -0400

    Add button to set size of grid

--- app.js
+++ app.js
@@ -7,12 +7,28 @@ function createSquare() {
   container.appendChild(square);
 }
 
-for (let i = 0; i < 16 * 16; i++) {
-  createSquare();
-};
-
 // --- Hover effect ---
 const squares = document.querySelectorAll(".square");
 squares.forEach(square => square.addEventListener("mouseover", () => {
   square.classList.add("hovering");
 }))
+
+// --- Ask user input to set grid size ---
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+  const squares = document.querySelectorAll(".square");
+  if (squares) {
+    squares.forEach(square => container.removeChild(square))
+  };
+
+  let gridSize = prompt("Enter the number of square per sides to create the grid:");
+
+  while (gridSize <= 1 && gridSize > 100) {
+    alert(`${gridSize} is not a valid input. Try to enter a number between 2 and 100.`)
+    gridSize = prompt("Enter the number of square per sides to create the grid:");
+  };
+
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    createSquare();
+  };
+})
